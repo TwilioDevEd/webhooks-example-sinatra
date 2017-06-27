b5404fe3bd861a91dc08de4fa18696012c0144cd
José Oliveros <jose.oliveros.1983@gmail.com>
2017-06-27 14:31:49 -0500

    update code for twilio-ruby 5.x

--- app.rb
+++ app.rb
@@ -11,11 +11,11 @@ post '/voice/?' do
   from_number = params[:From]
 
   # create a new TwiML response
-  response = Twilio::TwiML::Response.new do |r|
-    # <Say> a message to the caller
-    r.Say "Thanks for calling! Your phone number is #{from_number}. I got your call because of Twilio's webhook. Goodbye!", :voice => 'alice', :language => 'en-gb'
-  end
-  response.text
+  response = Twilio::TwiML::VoiceResponse.new
+  # <Say> a message to the caller
+  r.say "Thanks for calling! Your phone number is #{from_number}. I got your call because of Twilio's webhook. Goodbye!", :voice => 'alice', :language => 'en-gb'
+
+  response.to_xml_str
 end
 
 # SMS Request URL - receives incoming messages from Twilio
@@ -24,12 +24,11 @@ post '/message/?' do
   msg_length = params[:Body].length
 
   # <Message> a text bac to the person who texted us
-  response = Twilio::TwiML::Response.new do |r|
-    r.Sms  "Your text to me was #{msg_length} characters long. Webhooks are neat :)"
-  end
+  response = Twilio::TwiML::VoiceResponse.new
+  r.sms  "Your text to me was #{msg_length} characters long. Webhooks are neat :)"
 
   # Return the TwiML
-  response.text
+  response.to_xml_str
 end
 
 # Authenticate that all requests to our public-facing TwiML pages are
@@ -39,7 +38,7 @@ def validate_twilio_request
   twilio_signature = request.env['HTTP_X_TWILIO_SIGNATURE'] || 'invalid'
 
   # Helper from twilio-ruby to validate requests.
-  @validator = Twilio::Util::RequestValidator.new ENV['TWILIO_AUTH_TOKEN'] 
+  @validator = Twilio::Security::RequestValidator.new ENV['TWILIO_AUTH_TOKEN']
 
   # the POST variables attached to the request (eg "From", "To")
   # Twilio requests only accept lowercase letters. So scrub here:
@@ -49,7 +48,9 @@ def validate_twilio_request
 
   unless is_twilio_req
     content_type 'text/xml'
-    response = (Twilio::TwiML::Response.new {|r| r.Hangup}).text
+    response = Twilio::TwiML::VoiceResponse.new
+    response.hangup
+    response.to_xml_str
     halt 401
   end
 end
